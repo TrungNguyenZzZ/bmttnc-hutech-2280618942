444eeaf2a9405dbad244356e2376416a76e001ea
TrungNguyenZzZ <nguyentaitrung26704@gmail.com>
2025-05-27 00:25:23 +0700

    Lam not bai lab_02

--- lab_02/cipher/caesar/caesar_cipher.py
+++ lab_02/cipher/caesar/caesar_cipher.py
@@ -1,9 +1,7 @@
 from cipher.caesar import ALPHABET
-
 class CaesarCipher:
     def __init__(self):
         self.alphabet = ALPHABET
-
     def encrypt_text(self, text: str, key: int) -> str:
         alphabet_len = len(self.alphabet)
         text = text.upper()
@@ -15,10 +13,8 @@ class CaesarCipher:
                 output_letter = self.alphabet[output_index]
                 encrypted_text.append(output_letter)
             else:
-                encrypted_text.append(letter)  # Keep non-alphabetic characters
-
+                encrypted_text.append(letter)  
         return "".join(encrypted_text)
-
     def decrypt_text(self, text: str, key: int) -> str:
         alphabet_len = len(self.alphabet)
         text = text.upper()
